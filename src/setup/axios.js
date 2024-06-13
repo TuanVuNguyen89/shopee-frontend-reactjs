cd5c8788e27d4f7b552cd0355fec20ac1906217e
Nguyen Vu <nguyentuanvuk62@gmail.com>
2024-06-14 02:21:14 +0700

    config axios

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://shopee-backend-dx56.onrender.com'
 });
 
 instance.defaults.withCredentials = true;
